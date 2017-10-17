e78cfbc8eec5a13542e22553cf59e216a4a1066b
rubenpraets <rubenpraets@hotmail.com>
2017-10-17 12:31:15 +0200

    opgeslagen

--- CarRental-ejb/src/java/session/CarRentalSession.java
+++ CarRental-ejb/src/java/session/CarRentalSession.java
@@ -1,9 +1,11 @@
 package session;
 
+import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import javax.ejb.Stateful;
-import rental.RentalStore;
+import rental.*;
 
 @Stateful
 public class CarRentalSession implements CarRentalSessionRemote {
@@ -11,6 +13,26 @@ public class CarRentalSession implements CarRentalSessionRemote {
     @Override
     public Set<String> getAllRentalCompanies() {
         return new HashSet<String>(RentalStore.getRentals().keySet());
+    }
+    
+    @Override
+    public void confirmQuotes(){
+        
+    }
+    
+    @Override
+    public Quote createQuote(String clientName, Date start, Date end,
+			String carType, String region){
+        ReservationConstraints constraints = new ReservationConstraints(start, end, carType, region);
+        for(CarRentalCompany crc: RentalStore.getRentals().values()){
+            Quote quote = crc.createQuote(constraints, clientName);
+        }
+        return quote;
+    }
+
+    @Override
+    public List<Quote> getCurrentQuotes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     
